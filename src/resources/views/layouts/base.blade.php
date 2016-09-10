1dbb6e86c9aa22a8e6f8ab383fccd95528c22965
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2016-09-11 06:35:10 +0900

    ヘッダーフッターを設置

--- src/resources/views/layouts/base.blade.php
+++ src/resources/views/layouts/base.blade.php
@@ -2,12 +2,19 @@
 <html>
     <head>
         <title>@yield('title')</title>
-        <script src="public/js/jquery-3.1.0.min.js"></script>
-        <link rel="stylesheet" href="/public/css/base.css">
+        <script src="/js/jquery-3.1.0.min.js"></script>
+        <link rel="stylesheet" href="/css/base.css">
     </head>
     <body>
+        <header>
+        </header>
         <div class="container">
             @yield('content')
         </div>
+        <footer>
+            <div class="bottom">
+                @@copy2016 FM本州
+            </div>
+        </footer>
     </body>
 </html>
