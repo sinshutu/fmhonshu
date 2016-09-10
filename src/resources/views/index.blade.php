e09402ba3e36be073c5690fbe132c744b68ca86f
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2016-09-11 06:11:00 +0900

    html分割

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,48 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.base')
+@section('title', 'Page Title')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <form action="search" method="get">
-                    <input type="text" name="q" />
-                    <input type="submit" value="検索" />
-                </form>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+<div class="content">
+    <form action="search" method="get">
+        <input type="text" name="q" />
+        <input type="submit" value="検索" />
+    </form>
+</div>
+@endsection
---
+++ src/resources/views/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <script src="public/js/jquery-3.1.0.min.js"></script>
+        <link rel="stylesheet" href="/public/css/base.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
