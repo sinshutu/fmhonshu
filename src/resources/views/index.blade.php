0e4eab6d34b7b3b6312f8f14ad2962151342e164
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-09-11 09:00:43 +0900

    [update] updated index

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.base')
 @section('title', 'Page Title')
 
+@section('head')
+<link rel="stylesheet" href="/css/index.css">
+@endsection
+
 @section('content')
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
@@ -13,12 +17,12 @@
                 <tr>
                     <td>
                         <div class="issuer">
-                            <img src="/img/index/button_designer.png" />
+                            <img src="/img/index/button_designer.png"/>
                         </div>
                     </td>
                     <td>
                         <div class="issuer">
-                            <img src="/img/index/button_engineer.png" />
+                            <img src="/img/index/button_engineer.png"/>
                         </div>
                     </td>
                 </tr>
@@ -26,36 +30,43 @@
         </div>
 
         <div class="query">
-            <div id="query-inner">
-                <div id="category">
-                    <p>聞きたいことのカテゴリを選んでね</p>
+            <form>
+                <div id="query-inner">
+                    <div id="categories">
+                        <p>聞きたいことのカテゴリを選んでね</p>
 
-                    <div class="form-group">
-                    <table>
-                        @foreach ($categories as $category)
-                        <tr>
-                            <td>
-                                <div class="category">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" value="{{ $category->id }}" > {{ $category->name }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
+                        <div class="form-group">
+                            <table>
+                                @for ($i = 0; $i < count($categories); $i++)
+                                <tr>
+                                    <td>
+                                        <div class="category">
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="{{ $categories[$i]->id }}"> {{ $i }} {{
+                                                    $categories[$i]->name }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endfor
+                            </table>
+                        </div>
+
+                        <hr class="divider" />
+
                     </div>
 
-                </div>
-            </div>
-        </div>
+                    <div id="keywords">
+                        <p>聞きたいワード</p>
 
-        <div class="content">
-            <form action="search" method="get">
-                <input type="text" name="q" />
-                <input type="submit" value="検索" />
+                        <div id="keywords-form" class="form-group">
+                            <input type="text" class="form-control" name="keywords"/>
+                            <button type="submit" id="submit" class="btn btn-default">聞いてみる！</button>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
