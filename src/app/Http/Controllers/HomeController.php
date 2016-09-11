62f8e0f74c9d91626d34655759b4a63e73a36e8a
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2016-09-11 12:11:55 +0900

    ダミーデータ作れない

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\MinorCategory;
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -31,5 +32,14 @@ class HomeController extends Controller
         $request->flash();
         return view('search')
             ->with("posts", $posts);
+    }
+    public function db(){
+        $r = DB::delete( DB::raw("DELETE FROM posts;") );
+        $r = DB::insert( DB::raw("insert into `posts` (issuer_kind, title, circumstance, description, updated_at, created_at)
+            values (0, 'コピーするだけじゃないのか', 'ニュースアプリのデザインの変更で、マイページ画面にAと情報を出す機能がすでにあるのですが、それを記事の画面にも出して欲しいといったら簡単には出来ないと言われた。もうあるのだからそれをコピーすればいいだけだと思うのに理解できない', 'Aという情報がユーザ本人に紐づくもので本人のみ閲覧出来るページであるという状況下で表示しているものであると、記事という全てのユーザが閲覧出来る機能に個々のユーザの属性をもたせるということは新しい機能を作るのと同じこととなる。', now(), now());") );
+
+        $r = DB::select( DB::raw("SELECT * FROM posts;") );
+        dd($r);
+        return view('index')->with("categories", $categories);
     }
 }
